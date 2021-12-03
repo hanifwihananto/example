e06c1be9191d07770a4c4a3c224a4b7ba3e2e966
Hanif <hanifabid19@gmail.com>
2021-12-03 10:23:09 +0700

    component dan properties

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Component -> Properties
+
+function Biodata(props){
+  return <span> Usia {props.age} </span>
+}
+
+function Greeting(props){
+  return <h1> Hallo {props.name} - < Biodata age={props.age}/> </h1>
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Greeting name="Hanif" age="17"/>
+        <Greeting name="Nova" age="16"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -11,19 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-const nama = "Hanif Wihananto";
-
-function greet(){
-  return "Hallo selamat datang " + nama
-}
-
-const element = <h1> {greet(nama)} </h1>;
-
-ReactDOM.render(
-  element,
-  document.getElementById('master')
-);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
