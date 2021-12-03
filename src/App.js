1a02d1d026dde799e872fb443dfbf1b09901e9e0
Hanif <hanifabid19@gmail.com>
2021-12-03 14:17:03 +0700

    handle event

--- src/App.js
+++ src/App.js
@@ -2,47 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
-// State
-
-class Timer extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      time: props.start
-    }
-  }
-  
-  // Lifecycle
-  componentDidMount(){
-    this.addInterval = setInterval( () => this.increase(), 1000);
+function Clicker(){
+  function handleClick(e){
+    alert('Berhasil Mengklik !')
+    e.preventDefault()
   }
 
-  componentWillUnmount(){
-    clearInterval(this.addInterval)
-  }
-
-  increase(){
-    // Update State Setiap Detik 
-    this.setState((state, props) => ({
-      time: parseInt(state.time) + 1
-    }))
-  }
-
-  render(){
-    return (
-      <div> {this.state.time} Detik </div> 
-    )
-  }
+  return (
+    <a href="#" onClick={handleClick}> Klik Bro </a>
+  )
 }
-
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Timer start="0"/>
+        <Clicker />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
